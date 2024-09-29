dfd8243928fcabfeed0fb036d9284de5f62c63a0
Ritabrata Ghosh <76sonali40@gmail.com>
2024-09-29 11:30:09 +0530

    initial setup for backend

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import connectDB from "./db/index.js";
+
+
+connectDB();
+
+/*
+import express from "express";
+import { error } from "console";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", () => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
